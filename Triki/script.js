6d2148e1fb04b59a5d448f16545505ac80bbb4ff
Agustín <gaara238@outlook.es>
2024-11-11 10:09:36 -0500

    Se creo el boton de guardar que fue conectado a una funcion llamada saveFigure que sirve para que se insertara la figura donde los inputs de posicion x y posicion y indicaran en la matriz de Triki

--- Triki/script.js
+++ Triki/script.js
@@ -3,3 +3,12 @@ let Triki = [
     [null, null, null],
     [null, null, null]
 ];
+
+function saveFigure() {
+    let figure = document.getElementById("figure").value;
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
+    Triki[x][y] = figure;
+    console.log(Triki);
+    
+}
