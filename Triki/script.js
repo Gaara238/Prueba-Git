afdde92dfe7f6336ca566c525ef04400e740c777
Agustín <gaara238@outlook.es>
2024-11-11 10:18:11 -0500

    Se crearon dos condiciones para regular que las posiciones insertadas en los inputs si correspondan a las de la matriz

--- Triki/script.js
+++ Triki/script.js
@@ -8,7 +8,13 @@ function saveFigure() {
     let figure = document.getElementById("figure").value;
     let x = document.getElementById("x").value;
     let y = document.getElementById("y").value;
-    Triki[x][y] = figure;
-    console.log(Triki);
-    
+    if (x >= 0 && x <= 2) {
+        if (y >= 0 && y <= 2) {
+            Triki[x][y] = figure;
+        } else {
+            alert("Los caracteres insertados no coinciden con ninguna posicion de la tabla del triki, intente poner unos que si coincidan");
+        }
+    } else {
+        alert("Los caracteres insertados no coinciden con ninguna posicion de la tabla del triki, intente poner unos que si coincidan");
+    }
 }
