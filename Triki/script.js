5dfc58f0b07fc56002d0d024de765128d38349cf
Agustín <gaara238@outlook.es>
2024-11-12 13:17:37 -0500

    Hice una condicion para regular que el input de la figura solo acepte el caracter X o O

--- Triki/script.js
+++ Triki/script.js
@@ -8,13 +8,17 @@ function saveFigure() {
     let figure = document.getElementById("figure").value;
     let x = document.getElementById("x").value;
     let y = document.getElementById("y").value;
-    if (x >= 0 && x <= 2) {
-        if (y >= 0 && y <= 2) {
-            Triki[x][y] = figure;
+    if (figure === "X" || figure === "O" || figure === "o" || figure === "x") {
+        if (x >= 0 && x <= 2) {
+            if (y >= 0 && y <= 2) {
+                Triki[x][y] = figure;
+            } else {
+                alert("Los numeros insertados no coinciden con ninguna posicion de la tabla del triki, intente poner unos que si coincidan");
+            }
         } else {
-            alert("Los caracteres insertados no coinciden con ninguna posicion de la tabla del triki, intente poner unos que si coincidan");
+            alert("Los numeros insertados no coinciden con ninguna posicion de la tabla del triki, intente poner unos que si coincidan");
         }
     } else {
-        alert("Los caracteres insertados no coinciden con ninguna posicion de la tabla del triki, intente poner unos que si coincidan");
+        alert("La figura insertada no es X o O intente con una de estas figuras");
     }
 }
