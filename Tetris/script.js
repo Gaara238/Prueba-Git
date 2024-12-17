9967d811c2a8e09dc9ca6752799b74fdc7a49e50
Agustín <gaara238@outlook.es>
2024-12-17 13:57:28 -0500

    Estamos trabajando en la carga del tablero

---
+++ Tetris/script.js
@@ -0,0 +1,71 @@
+let canvas = document.getElementById('canvas');
+let context = null;
+
+//Configuración de tamaño del tablero
+let rows = 20;
+let columns = 10;
+let blockSize = 30;
+
+//Tablero
+let board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+];
+
+let square = [[1, 1], [1, 1]];
+let rectangle = [[1, 1, 1, 1]];
+let l = [[1], [1, 1, 1]];
+let j = [[0, 0, 1], [1, 1, 1]];
+let s = [[0, 1, 1], [1, 1]];
+let z = [[1, 1], [0, 1, 1]];
+let t = [[0, 1, 0], [1, 1, 1]];
+
+function drawBoard() {
+    console.log(context);
+    context.clearRect(0, 0, canvas.width, canvas.heigth);
+    for (let i = 0; i < board.length; i++) {
+        for (let y = 0; y < board.length; y++) {
+            if (board[i][y]) {
+                console.log(i, y);
+                
+                drawBlock(i, y, "green");
+            }
+        }
+    }
+}
+
+function drawBlock(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    context.strokeStyle = '#222';
+    context.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+}
+
+function update() {
+    drawBoard();
+    requestAnimationFrame(update);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext("2d");
+    update();
+});
