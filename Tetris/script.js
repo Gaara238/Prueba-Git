3d490fa389bdd6028c657be68ea690fe0b6ef9b1
Agustín <gaara238@outlook.es>
2024-12-19 20:42:13 -0500

    Se crearon dos funciones para generar y cargar una pieza aleatoria

--- Tetris/script.js
+++ Tetris/script.js
@@ -38,6 +38,16 @@ let s = [[0, 1, 1], [1, 1]];
 let z = [[1, 1], [0, 1, 1]];
 let t = [[0, 1, 0], [1, 1, 1]];
 
+let pieces = [square, rectangle, l, j, s, z, t];
+
+let centralPosition = {
+    x: 4,
+    y: 0
+};
+
+let currentPiece = getRandomPiece();
+
+
 function drawBoard() {
     context.clearRect(0, 0, canvas.width, canvas.heigth);
     for (let i = 0; i < board.length; i++) {
@@ -58,12 +68,27 @@ function drawBlock(x, y, color) {
 
 function update() {
     drawBoard();
+    drawPiece(currentPiece, centralPosition);
     requestAnimationFrame(update);
+}
+
+function getRandomPiece() {
+    let index = Math.floor(Math.random() * pieces.length);
+    return pieces[index];
+}
+
+function drawPiece(piece, position) {
+    for (let i = 0; i < piece.length; i++) {
+        for (let y = 0; y < piece[i].length; y++) {
+            if (piece[i][y] === 1) {
+                drawBlock(position.x + y, position.y + i, "red");
+            }
+        }
+    }
 }
 
 window.addEventListener("DOMContentLoaded", function () {
     canvas = document.getElementById('canvas');
     context = canvas.getContext("2d");
     update();
-
 });
