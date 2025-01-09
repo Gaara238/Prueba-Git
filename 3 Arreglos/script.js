572a5af17439ba40bb893657cad9bd76c72e3305
Agustín <gaara238@outlook.es>
2025-01-09 13:25:58 -0500

    Se optimizo la forma en la que escribimos el codigo

--- 3 Arreglos/script.js
+++ 3 Arreglos/script.js
@@ -1,27 +1,18 @@
 let array1 = [];
 let array2 = [];
 let array3 = [];
-let numberRandom = null;
 
 function randomNumber() {
-  numberRandom = Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * 100);
 }
 
 for (let i = 0; i < 10; i++) {
-    randomNumber();
-  array1[i] = numberRandom;
-}
-
-for (let i = 0; i < 10; i++) {
-    randomNumber();
-  array2[i] = numberRandom;
-}
-
-for (let i = 0; i < 10; i++) {
-  array3[i] = array1[i] + array2[i];
+  array1.push(randomNumber());  
+  array2.push(randomNumber());
+  //Como ya se creo la posición en ambos arreglos ahora si podemos acceder a ellas  
+  array3.push(array1[i] + array2[i]);
 }
 
 console.log(array1);
 console.log(array2);
 console.log(array3);
-
