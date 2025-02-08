61b194fcc486f9414e4bc1ef31924e030ce1ed0e
Agustín <gaara238@outlook.es>
2025-02-08 09:03:55 -0500

    Se esta trabajando en la función save

---
+++ parking/script.js
@@ -0,0 +1,8 @@
+class Vehicle {
+    constructor (name, plate, vehicleType, vip) {
+        this.name = name;
+        this.plate = plate;
+        this.vehicleType = vehicleType;
+        this.vip = vip;
+    }
+}
