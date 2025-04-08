36ee98733b5373f10907f735f753806b8cf276d3
Agustín <gaara238@outlook.es>
2025-04-08 14:12:00 -0500

    Se ha creado una matriz 4x4 que responde a las dimensiones del tablero de juego, tambien se ha creado una clase que tiene dos atributos: img y position, estas son las bases de las tarjetas. Tambien se ha creado una funcion que crea instancias de la clase y junto con las funciones chooseImg (crea un numero random del 0 al 7 y a cada numero se le asigna una imagen y despues se retorna a la funcion createPair) y la funcion choosePosition (hace lo mismo que chooseImg solo que un poco diferente) crean 2 instancias, las 2 con la misma imagen, pero con diferente posision en la matriz y por ultimo estas instancias se envian a un arreglo vacio llamado pairs previamente creado

---
+++ Juego de pares/script.js
@@ -0,0 +1,25 @@
+let board = [
+  [null, null, null, null],
+  [null, null, null, null],
+  [null, null, null, null],
+  [null, null, null, null],
+];
+
+class Pair {
+  constructor(img, position1, position2) {
+    this.img = img;
+    this.position1 = position1;
+    this.position2 = position2;
+  }
+}
+
+function createPair() {
+    let img = 
+}
+
+function chooseImg() {
+    let randomNumber = Math.floor(Math.random() * 7);
+    if (randomNumber === 0) {
+      return ""
+    }
+}
