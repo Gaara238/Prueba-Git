89aedc0bfcd2aa8507fc8b3a41e650892dbd4d2a
Agustín <gaara238@outlook.es>
2025-06-24 13:50:18 -0500

    Se termino el ejercicio solucionando todos sus bugs

--- anagramas/script.js
+++ anagramas/script.js
@@ -1,57 +1,26 @@
 function sonAnagramas(str1, str2) {
   str1 = str1.toLowerCase();
   str2 = str2.toLowerCase();
-  let entry = false;
-  let countLeters1 = 0;
-  let countSymbols1 = 0;
+  let str1Aux = "";
+  let str2Aux = "";
   for (let i = 0; i < str1.length; i++) {
-    let ascci1 = str1[i].charCodeAt();
-    if (ascci1 > 96 && ascci1 < 123) {
-      countLeters1++;
-    } else {
-      countSymbols1++;
+    if (str1[i].charCodeAt() > 96 && str1[i].charCodeAt() < 123) {
+      str1Aux += str1[i];
     }
   }
-  let sum1 = 0;
-  if (countSymbols1 > 0) {
-    sum1 = countLeters1 + countSymbols1;
-  }
-  if (sum1 === str1.length) {
-    entry = true;
-  } else {
-    entry = false;
-  }
-  let countLeters2 = 0;
-  let countSymbols2 = 0;
+  str1 = str1Aux;
   for (let i = 0; i < str2.length; i++) {
-    let ascci2 = str2[i].charCodeAt();
-    if (ascci2 > 96 && ascci2 < 123) {
-      countLeters2++;
-    } else {
-      countSymbols2++;
+    if (str2[i].charCodeAt() > 96 && str2[i].charCodeAt() < 123) {
+      str2Aux += str2[i];
     }
   }
-  let sum2 = 0;
-  if (countSymbols2 > 0) {
-      sum2 = countLeters2 + countSymbols2;
-  }
-  if (sum2 === str2.length) {
-    entry = true;
-  } else {
-    entry = false;
-  }
-  if (str1.length === str2.length || entry) {
+  str2 = str2Aux;
+  if (str1.length === str2.length) {
     let check1 = false;
     let count1 = 0;
     for (let i = 0; i < str1.length; i++) {
-      let ascci = str1[i].charCodeAt();
       for (let j = 0; j < str2.length; j++) {
-        if (ascci > 96 && ascci < 123) {
-          if (str1[i] === str2[j]) {
-            count1++;
-            break;
-          }
-        } else {
+        if (str1[i] === str2[j]) {
           count1++;
           break;
         }
@@ -65,14 +34,8 @@ function sonAnagramas(str1, str2) {
     let check2 = false;
     let count2 = 0;
     for (let i = 0; i < str2.length; i++) {
-      let ascci = str2[i].charCodeAt();
       for (let j = 0; j < str1.length; j++) {
-        if (ascci > 96 && ascci < 123) {
-          if (str2[i] === str1[j]) {
-            count2++;
-            break;
-          }
-        } else {
+        if (str2[i] === str1[j]) {
           count2++;
           break;
         }
@@ -93,4 +56,4 @@ function sonAnagramas(str1, str2) {
   }
 }
 
-sonAnagramas("programar", "gramarpro");
+sonAnagramas("hola", "adios");
