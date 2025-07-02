d8c180656f568d9898f36be3703084434b9a8611
Agustín <gaara238@outlook.es>
2025-07-02 12:52:37 -0500

    Se creo un codigo que identifica que numeros se repiten en dos arreglos sin que se repita ningun valor

---
+++ interseccion/script.js
@@ -0,0 +1,21 @@
+function interseccion(arr1, arr2) {
+    let equalData = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                let exist = false;
+                for (let l = 0; l < equalData.length; l++) {
+                    if (arr1[i] === equalData[l]) {
+                        exist = true;
+                    }
+                }
+                if (!exist) {
+                    equalData.push(arr1[i]);
+                }
+            }
+        }
+    }
+    console.log(equalData);
+}
+
+interseccion([1, 2, 3], [3, 4, 5, 3]);
